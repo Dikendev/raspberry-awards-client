bc4a061e575c4a7754c5c1b791a23bfcaed71439
Dikendev <diken.dev@gmail.com>
2024-08-17 21:48:11 -0300

    Chore: Add modal styles and update routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import About from './pages/About';
+import RaspberryAwards from './pages/RaspberyAwards';
+import Home from './pages/Home';
+import AnalyticsPage from './pages/AnalyticsPage';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/raspberry" element={<RaspberryAwards />} />
+        <Route path="/analytics" element={<AnalyticsPage />} />
+        <Route path="/about" element={<About />} />
+        {/* <Route path="/contact" element={<Contact />} /> */}
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
